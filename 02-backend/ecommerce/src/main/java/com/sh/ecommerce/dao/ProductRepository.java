d0fd3672de82a5de2353f67aab5ad17e3084dc35
midweekvermin <midweekvermin@gmail.com>
2024-07-08 09:26:54 -0500

    update notes on ProductRepository.java

--- 02-backend/ecommerce/src/main/java/com/sh/ecommerce/dao/ProductRepository.java
+++ 02-backend/ecommerce/src/main/java/com/sh/ecommerce/dao/ProductRepository.java
@@ -5,3 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
+/*
+*The ProductCategoryRepository extends JpaRepository,
+which provides CRUD operations (Create, Read, Update, Delete) and query methods.
+*Spring Data JPA automatically implements these methods,
+so you don't have to write any boilerplate code.
+ */
