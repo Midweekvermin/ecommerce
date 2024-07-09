ca549099083c16ce2407052920240dfef633100d
midweekvermin <midweekvermin@gmail.com>
2024-07-08 19:52:05 -0500

    set up crossorigin support, created and connected angular front end

--- 02-backend/ecommerce/src/main/java/com/sh/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/ecommerce/src/main/java/com/sh/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,10 @@ package com.sh.ecommerce.dao;
 import com.sh.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200") // allows calls from browser scrips from this origin
+//^ also fixes the Cors error
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 //exposes endpoint as /product-category
 //collectionResourceRel names Json returned productCategory
--- 02-backend/ecommerce/src/main/java/com/sh/ecommerce/dao/ProductRepository.java
+++ 02-backend/ecommerce/src/main/java/com/sh/ecommerce/dao/ProductRepository.java
@@ -2,7 +2,10 @@ package com.sh.ecommerce.dao;
 
 import com.sh.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200") // allows calls from browser scrips from this origin
+//^ also fixes the Cors error
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
 /*
