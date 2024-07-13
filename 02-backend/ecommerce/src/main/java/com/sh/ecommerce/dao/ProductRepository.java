816486e7208cc05085ffbb016227c28d2f3bd3f0
midweekvermin <midweekvermin@gmail.com>
2024-07-12 21:47:17 -0500

    category support added to the spring back end

--- 02-backend/ecommerce/src/main/java/com/sh/ecommerce/dao/ProductRepository.java
+++ 02-backend/ecommerce/src/main/java/com/sh/ecommerce/dao/ProductRepository.java
@@ -1,12 +1,27 @@
 package com.sh.ecommerce.dao;
 
 import com.sh.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+
 
 @CrossOrigin("http://localhost:4200") // allows calls from browser scrips from this origin
 //^ also fixes the Cors error
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+//    Page<Product>: Returns a paginated list of products.
+//    findByCategoryId: Finds products by the category ID.
+//    @Param("id") Long id: Maps the method parameter id to the query parameter id.
+//    Pageable pageable: Encapsulates pagination information,
+//    such as page number, page size, and sorting details.
+    // Springboot automatically creates a query in the background
+    // `SELECT * FROM product WHERE category_id=`
+    //exposes the /api/products/search/findByCategoryId endpoint
 }
 /*
 *The ProductCategoryRepository extends JpaRepository,
