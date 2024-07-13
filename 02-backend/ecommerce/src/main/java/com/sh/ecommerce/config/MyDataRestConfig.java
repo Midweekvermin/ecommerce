f9c93b7c28003d3e7fc031195b982a205a7aae27
midweekvermin <midweekvermin@gmail.com>
2024-07-13 14:54:56 -0500

    expose entity ids

--- 02-backend/ecommerce/src/main/java/com/sh/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/ecommerce/src/main/java/com/sh/ecommerce/config/MyDataRestConfig.java
@@ -2,14 +2,33 @@ package com.sh.ecommerce.config;
 
 import com.sh.ecommerce.entity.Product;
 import com.sh.ecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Configuration //alerts spring that it's a configuration class
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+//    The EntityManager is part of the Java
+//    Persistence API (JPA) and is used to interact with the persistence context
+//    It provides methods to create, read, update, and delete entities.
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+        // Allows class to use the EntityManager without having to manually instantiate it
+    }
+
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
@@ -40,5 +59,23 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        exposeIds(config);
+
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        // Get all entities from the EntityManager's metamodel
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        // Create a list to hold the entity classes
+        List<Class> entityClasses = new ArrayList<>();
+        // Iterate over the set of entities and add their Java types to the list
+        for (EntityType tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+        // Convert the list of entity classes to an array
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        // Configure the RepositoryRestConfiguration to expose the IDs for the entity classes
+        config.exposeIdsFor(domainTypes);
     }
 }
