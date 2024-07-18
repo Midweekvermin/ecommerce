791831c417308902c8053c788f836698ccb3b57d
midweekvermin <midweekvermin@gmail.com>
2024-07-18 14:18:34 -0500

    updated shop service to handle states and country arrays, created state and country classes

--- 03-frontend/angular-ecommerce/src/app/services/shop-form.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/shop-form.service.ts
@@ -1,12 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { Country } from '../common/country';
+import { map }  from 'rxjs/operators';
+import { State } from '../common/state';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopFormService {
 
-  constructor() { }
+  private countriesURL = 'http://localhost:8080/api/countries';
+  private statesURL = 'http://localhost:8080/api/states';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getCountries(): Observable<Country[]>{
+    return this.httpClient.get<GetResponseCountries>(this.countriesURL).pipe(map(
+      response => response._embedded.countries
+    ));
+  }
+  getStates(theCountryCode: string): Observable<State[]>{
+    const searchURL = `${this.statesURL}/search/findByCountryCode?code=${theCountryCode}`
+    return this.httpClient.get<GetResponseStates>(searchURL).pipe(map(
+      response => response._embedded.states
+    ));
+  }
 
   getCreditCardMonths(startMonth:number): Observable<number[]> {
 
@@ -26,5 +45,16 @@ export class ShopFormService {
       data.push(theYear);
     }
     return of(data); //of wraps the object as an observable
+  }
+}
+
+interface GetResponseCountries {
+  _embedded: {
+    countries: Country[];
+  }
+}
+interface GetResponseStates {
+  _embedded: {
+    states: State[];
   }
 }
